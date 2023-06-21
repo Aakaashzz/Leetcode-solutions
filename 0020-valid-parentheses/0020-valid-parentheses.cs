b458c78f2db126786bca520ea8983627db52b025
Aakaash <84792076+Aakaashzz@users.noreply.github.com>
2023-06-21 15:11:40 +0530

    Time: 84 ms (36.77%), Space: 37.9 MB (64.72%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public bool IsValid(string s) {
+        Dictionary<char, char> bracketsMap = new Dictionary<char, char>{
+            {'{',  '}'},
+            {'(',  ')'},
+            {'[',  ']'},
+        };
+        Stack<char> openBrackets = new Stack<char>();
+        
+        foreach (char bracket in s) {
+            if (bracketsMap.ContainsKey(bracket)) {
+                openBrackets.Push(bracket);
+            } else {
+                 if (openBrackets.Count == 0) {
+                    return false;
+                }
+                if (bracketsMap[openBrackets.Pop()] == bracket) {
+                    continue;
+                };
+                return false;
+            }
+        }
+        return openBrackets.Count == 0;
+    }
+}
